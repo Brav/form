b1935cb906c47edfc8975bda482c4856fbed496c
Brav <imalimalomrk@gmail.com>
2021-02-25 17:17:01 +0100

    updating

---
+++ resources/js/paginate.js
@@ -0,0 +1,22 @@
+$('body').on('click', '.page-link', function (e) {
+
+    e.preventDefault();
+
+    let $this      = $(this);
+    let pagination = $this.closest('ul');
+    let page       = parseInt($(this).data("page"));
+    let role       = $this.closest("ul").attr("role");
+    let data       = {}
+
+    $.get(
+        $this.attr("href"),
+        data,
+        function (data, textStatus, jqXHR) {
+
+            $("#" + pagination.data("container")).html(data.html);
+            $(`#pagination-${data.id}`).html(data.pagination);
+
+        },
+        "json"
+    );
+})
