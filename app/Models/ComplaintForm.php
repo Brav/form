418d652045147380224bbfac88e32ab6e729b4fc
Brav <imalimalomrk@gmail.com>
2021-04-15 21:03:10 +0200

    soft delete functionallity added to complaint forms

--- app/Models/ComplaintForm.php
+++ app/Models/ComplaintForm.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
 
 class ComplaintForm extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
