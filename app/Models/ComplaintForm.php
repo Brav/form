778aee035f188dace41aa08db4f2875fb48b1d90
Brav <imalimalomrk@gmail.com>
2021-04-27 23:13:52 +0200

    everything added

--- app/Models/ComplaintForm.php
+++ app/Models/ComplaintForm.php
@@ -63,7 +63,7 @@ class ComplaintForm extends Model
      */
     public function format(array $data, bool $update = false) :array
     {
-        $date = DateTime::createFromFormat('d/m/Y g:i A', $data['date_of_incident']);
+        $date = DateTime::createFromFormat('d/m/Y', $data['date_of_incident']);
 
         if(isset($data['date_of_incident']))
         {
