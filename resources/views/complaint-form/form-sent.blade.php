2a1ee5dec7e334dfbad3a2a0d19c597b8b08be53
DESKTOP-OVCDGTL\dushan <dushan887@gmail.com>
2021-11-08 13:48:14 +0100

    update

--- resources/views/complaint-form/form-sent.blade.php
+++ resources/views/complaint-form/form-sent.blade.php
@@ -4,21 +4,24 @@
 
     <div class="content">
         <div class="block-content">
-                <div class="alert alert-success" role="alert">
-                    <h4 class="alert-heading">Complaint Sent!</h4>
+            <div class="container">
+                <div class="alert alert-success text-center" role="alert">
+                    <hr>
+                    <h4 class="h1 alert-heading my-3">Complaint Sent!</h4>
                     <hr>
                     @if (session('response'))
                         {!! session('response') !!}
                     @endif
                 </div>
 
-                <p>
-                <h4>Useful resources for dealing with complaints</h4><br>
-                <span><a href="https://rise.articulate.com/share/ebvbTT1KJhQKagctvBlQGhcH1F5ACXN8#/">Difficult Conversation Planning </a></span><br>
-                    <span><a href="https://rise.articulate.com/share/9WMRd5wNr9CVkiIlFSWRnI3TQlW6NMX7#/">How to Have a Difficult Conversation</a></span><br>
-                    <span><a href="https://rise.articulate.com/share/IrnA9Lkdv_7s6S7QMrlV9BetXvHjidXl#/">Managing Client Complaints and Angry Customers</a></span><br>
-                    <span><a href="{{ route('operational_policy') }}">Clients Complaints and Concerns - Operational Policy</a></span><br>
-                </p>
+                <div class="text-center mb-5">
+                    <h4 class="h2 my-3">Useful resources for dealing with complaints</h4>
+                    <a class="btn btn-primary my-2 mx-auto d-block btn-lg" style="max-width: 600px" href="https://rise.articulate.com/share/ebvbTT1KJhQKagctvBlQGhcH1F5ACXN8#/">Difficult Conversation Planning</a>
+                    <a class="btn btn-primary my-2 mx-auto d-block btn-lg" style="max-width: 600px" href="https://rise.articulate.com/share/9WMRd5wNr9CVkiIlFSWRnI3TQlW6NMX7#/">How to Have a Difficult Conversation</a>
+                    <a class="btn btn-primary my-2 mx-auto d-block btn-lg" style="max-width: 600px" href="https://rise.articulate.com/share/IrnA9Lkdv_7s6S7QMrlV9BetXvHjidXl#/">Managing Client Complaints and Angry Customers</a>
+                    <a class="btn btn-primary my-2 mx-auto d-block btn-lg" style="max-width: 600px" href="{{ route('operational_policy') }}">Clients Complaints and Concerns - Operational Policy</a>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
