22b63d7dd602b9c39272bbf280c97b7c5fab416d
Brav <imalimalomrk@gmail.com>
2023-01-23 09:06:22 +0100

    WIP

--- app/Providers/SendEmailToManagers.php
+++ app/Providers/SendEmailToManagers.php
@@ -47,7 +47,7 @@ class SendEmailToManagers
 
         $mailTo = array_merge($managers->pluck('email')->toArray(), $autoResponse->additional_emails ?? []);
 
-        $mailTo = \filter_var_array($mailTo, FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL);
+        $mailTo = \array_filter(\filter_var_array($mailTo, FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL));
 
         dd($mailTo);
 
