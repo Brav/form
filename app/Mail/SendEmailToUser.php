02c462dfcfc78b8308fbb970db09b8e3eaba1e6f
Brav <imalimalomrk@gmail.com>
2023-08-25 10:22:44 +0200

    WIP

--- app/Mail/SendEmailToUser.php
+++ app/Mail/SendEmailToUser.php
@@ -36,8 +36,6 @@ class SendEmailToUser extends Mailable
      */
     public function build()
     {
-        dd($this->autoCountryEmails);
-
         return $this->from('complaintsreporting@vet.partners')
             ->subject('Complaint Form Filled')
             ->view('emails/complaint-form-user')
