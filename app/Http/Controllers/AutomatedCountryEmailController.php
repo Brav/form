fc0d861dce5e6935d352905ebb47dcb66eb92731
Brav <imalimalomrk@gmail.com>
2023-09-05 08:37:57 +0200

    WIP

--- app/Http/Controllers/AutomatedCountryEmailController.php
+++ app/Http/Controllers/AutomatedCountryEmailController.php
@@ -60,6 +60,8 @@ class AutomatedCountryEmailController extends Controller
         $data['body']['client'] = \strip_tags($data['body']['client'], '<p><a><bold><strong><em><i>');
         $data['body']['clinic'] = \strip_tags($data['body']['clinic'], '<p><a><bold><strong><em><i>');
 
+        $data['emails'] ??= '';
+
         $response = AutomatedCountryEmail::create($data);
 
         return response()->json(
