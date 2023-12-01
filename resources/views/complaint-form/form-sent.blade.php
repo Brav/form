d5b5600f4183bf740b146eff6811510972344138
Brav <imalimalomrk@gmail.com>
2023-12-01 09:02:13 +0100

    WIP

--- resources/views/complaint-form/form-sent.blade.php
+++ resources/views/complaint-form/form-sent.blade.php
@@ -27,11 +27,11 @@
                 <p>
                 <h4>Useful resources for dealing with complaints</h4><br>
                 <ul class=list-group>
-                    <li class="list-group-item"><a href="<?php echo asset('media/documents/THE-DO_S-AND-DON_T-OF-COMPLAINT-MANAGEMENT-v2.pdf') ?>">Do’s & Don’ts of Complaint Management</a></li>
+                    <li class="list-group-item"><a href="https://vetpartners.sharepoint.com/sites/Intranet-VetServices/SitePages/Complaints.aspx">Do’s & Don’ts of Complaint Management</a></li>
                     <li class="list-group-item"><a href="https://rise.articulate.com/share/ebvbTT1KJhQKagctvBlQGhcH1F5ACXN8#/">Difficult Conversation Planning </a></li>
                     <li class="list-group-item"><a href="https://rise.articulate.com/share/9WMRd5wNr9CVkiIlFSWRnI3TQlW6NMX7#/">How to Have a Difficult Conversation</a></li>
                     <li class="list-group-item"><a href="https://rise.articulate.com/share/IrnA9Lkdv_7s6S7QMrlV9BetXvHjidXl#/">Managing Client Complaints and Angry Customers</a></li>
-                    <li class="list-group-item"><a href="{{ route('operational_policy') }}">Clients Complaints and Concerns - Operational Policy</a></li>
+                    <li class="list-group-item"><a href="https://vetpartners.sharepoint.com/sites/Intranet-VetServices/SitePages/Complaints.aspx">Clients Complaints and Concerns - Operational Policy</a></li>
                     <li class="list-group-item"><a href="<?php echo asset('media/documents/responding_to_reviews.pdf') ?>" class="d-block" target=_blank rel=noopener rel=nofollow>
                             <i class="fas fa-file-pdf fa-lg fa-fw"></i> Responding To Reviews
                         </a></li>
