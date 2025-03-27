3845e740135d72fbe61fc0df656b4ae4c9a5ee65
Brav <imalimalomrk@gmail.com>
2025-03-27 17:56:08 +0100

    UI updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,11 +68,12 @@
             <!-- end slide -->
         </div>
 
-        <div class="ds-seperator">
+        {{-- <div class="ds-seperator">
 
             <a class="page-scroll fusion-one-page-text-link" href="#">
                 <div class="lineholderfirst"></div>
             </a>
+
 
             <div class="lineholder">
                 <svg version="1.1" class="firstdivider" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0.5px" y="0.5px" width="2560px" height="100px" viewBox="0 0 2560 100" style="enable-background:new 0 0 2560 100;" xml:space="preserve">
@@ -83,7 +84,7 @@
 
             </div>
 
-        </div>
+        </div> --}}
 
     </div>
 @endsection
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<section class="d-none d-sm-none d-md-block">
+{{-- <section class="d-none d-sm-none d-md-block">
 	<img  title="line1g" src="{{ asset('media/svg/line1g.svg')}}" class="img-fluid" style="margin-bottom: -2px;">
 </section>
 <footer id="page-footer" class="bg-primary-dark pt-5">
@@ -28,6 +28,69 @@
         		<p class="text-white text-center">Unit 1, 277 Lane Cove Road, Macquarie Park, NSW 2113 ACN 114 962 453</p>
         	</div>
         </div>
+    </div>
+</footer> --}}
+
+<footer id="footer-new">
+    <div class="footer-new-top">
+
+        <div class="footer-new-column">
+
+            <img src="{{ asset('media/logos/vetpartners-logo-1x.png') }}">
+
+            <p>Join Us. Grow Together</p>
+
+            <div class="footer-new-social-networks">
+                <div class="footer-new-social-network">
+                    <a rel="noopener noreferrer" href="https://www.facebook.com/vetpartnersanz/" target="_blank">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+
+                </div>
+                <div class="footer-new-social-network">
+                    <a rel="noopener noreferrer" href="https://www.instagram.com/vetpartners/" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+                <div class="footer-new-social-network">
+                    <a rel="noopener noreferrer" href="https://www.linkedin.com/company/7792003/" target="_blank">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer-new-column footer-new-contact">
+            <p>Contact Us</p>
+
+            <ul>
+                <li>
+                    <i class="fas fa-phone-alt"></i>
+                    <span>
+                        Australia:
+                        <br>
+                        <a href="tel:1800 881 864">1800 881 864</a>
+                    </span>
+                </li>
+                <li>
+                    <i class="fas fa-phone-alt"></i>
+                    <span>
+                        New Zealand:
+                        <br>
+                        <a href="tel:0800 426 488">0800 426 488</a>
+                    </span>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+    <div class="footer-new-bottom">
+        <p>
+            <a href="https://vet.partners//privacy-policy/">Privacy Policy</a> |
+            <a href="https://vet.partners//privacy-collection-notice/">Privacy Collection Notice</a> |
+            <a href="https://vet.partners//terms-and-conditions/">Terms and Conditions</a>
+        </p>
+        <p>© {{ date('Y') }} VetPartners | All Rights Reserved</p>
     </div>
 </footer>
 <!-- END Footer -->
